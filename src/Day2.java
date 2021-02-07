0434f96488ed23ac55b43871eabe2b9bc6df2096
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-07 12:58:55 +0300

    Update Day2.java

--- src/Day2.java
+++ src/Day2.java
@@ -1,2 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+
 public class Day2 {
+  public static void main(String[] args) {
+    String str = "[[({})]]";
+  }
+
+  private static boolean isStringRight (String str) {
+    char[] arr = str.toCharArray();
+    HashMap<Character, Character> skobochki = new HashMap<>();
+    skobochki.put('(', ')');
+    skobochki.put('[', ']');
+    skobochki.put('{', '}');
+    Deque<Character> deq = new ArrayDeque<>();
+
+  }
+
 }
+
+
+//Задачка на строку из 6 разных скобок [{()}]
