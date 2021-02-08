8525e5c0ed1a1f0c8392553e68f8ef9c3e6b75dd
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-08 23:24:13 +0300

    FizzBuzz

--- src/Day3/Task2.java
+++ src/Day3/Task2.java
@@ -2,19 +2,34 @@ package Day3;
 
 public class Task2 {
   public static void main(String[] args) {
-
+    fizzBuzz(15);
   }
 
   private static void fizzBuzz (int n) {
     if (n < 3) {
       throw new RuntimeException("N smaller then 3");
     }
-    for (int number = 3; number < n; number++) {
+    for (int number = 3; number <= n; number++) {
+      int fizzBuzz = 0;
       if (number % 3 == 0) {
-        System.out.print(number + ": Fizz");
+        fizzBuzz = 1;
       }
       if (number % 5 == 0) {
-        System.out.print(number + ": Buzz");
+        fizzBuzz = 2;
+      }
+      if (number % 3 == 0 && number % 5 == 0) {
+        fizzBuzz = 3;
+      }
+      switch (fizzBuzz) {
+        case 1 :
+          System.out.println(number + ": Fizz");
+          break;
+        case 2 :
+          System.out.println(number + ": Buzz");
+          break;
+        case 3 :
+          System.out.println(number + ": FizzBuzz");
+          break;
       }
     }
   }
