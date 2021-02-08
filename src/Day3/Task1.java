423cd0cffa72708924cbed115feacb3a8b59d657
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-09 00:11:20 +0300

    пытаемся доделать простые числа

--- src/Day3/Task1.java
+++ src/Day3/Task1.java
@@ -1,24 +1,32 @@
 package Day3;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 
 public class Task1 {
   public static void main(String[] args) {
-    System.out.println(getSimpleNumbers(10));
+    System.out.println(getSimpleNumbers(Integer.MAX_VALUE));
   }
 
 private static ArrayList<Integer> getSimpleNumbers (int n) {
-    ArrayList<Integer> result = new ArrayList<>();
-    for (int i = 2; i < n; i++) {
-      for (int j = 2; j <= i/2; j++) {
-        if (i%j == 0) {
-          break;
-        } else if (j == n/2) {
-          result.add(i);
+    ArrayList<Integer> foundedSimpleNumbers = new ArrayList<>();
+    long beginFunc = System.currentTimeMillis();
+    nextNumber:
+    for (int number = 2; number <= n; number++) {
+      for (int delitel = 2; delitel <= number/2; delitel++) {
+        if (number%delitel == 0) {
+          continue nextNumber;
         }
       }
+      foundedSimpleNumbers.add(number);
     }
-    return result;
+    System.out.println("Время выполнения функции "+(System.currentTimeMillis() - beginFunc));
+    return foundedSimpleNumbers;
+}
+
+private static boolean isSimple (int number, ArrayList<Integer> foundedSimpleNumbers) {
+
 }
 
 }
