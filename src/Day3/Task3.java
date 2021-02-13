117fabc4ae2239750d834c0131a858718b4a666a
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-13 23:16:27 +0300

    Matrix

--- src/Day3/Task3.java
+++ src/Day3/Task3.java
@@ -4,29 +4,72 @@ import java.util.ArrayList;
 
 public class Task3 {
   public static void main(String[] args) {
+    ArrayList<Integer> arr1 = new ArrayList<>();
+    ArrayList<Integer> arr2 = new ArrayList<>();
+    ArrayList<Integer> arr3 = new ArrayList<>();
+    arr1.add(0);
+    arr1.add(0);
+    arr1.add(0);
+    arr2.add(0);
+    arr2.add(0);
+    arr2.add(0);
+    arr3.add(0);
+    arr3.add(0);
+    arr3.add(0);
+    ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+    matrix.add(arr1);
+    matrix.add(arr2);
+    matrix.add(arr3);
 
+    System.out.println(isMatrixTrue(matrix));
   }
 
-  private static boolean isMatrixTrue (ArrayList<ArrayList<Integer>> matrix) {
+  private static boolean isMatrixTrue (ArrayList<ArrayList<Integer>> matrix){
     if (matrix.size() < 1) {
-      throw new IllegalArgumentException("size < 1");
+      throw new IllegalArgumentException("matrix size < 1");
     }
-    int sizeFirstArray = matrix.get(0).size();
+    for (ArrayList<Integer> line : matrix) {
+      if (line.size() != matrix.get(0).size()) {
+        throw new IllegalArgumentException("Matrix is broken");
+      }
+    }
+    if (isColumnsTrue(matrix) || isLinesTrue(matrix)) {
+      return true;
+    }
+    return false;
+  }
+  private static boolean isColumnsTrue (ArrayList<ArrayList<Integer>> matrix) {
+    ArrayList<Integer> column = new ArrayList<>();
+    for (int i = 0; i < matrix.size(); i++) {
+      column.add(matrix.get(i).get(0));
+      matrix.get(i).remove(0);
+    }
+    if (isListTrue(column)) {
+      return true;
+    }
 
-    for (ArrayList<Integer> arr : matrix) {
-      if (arr.size() != sizeFirstArray) {
-        throw new IllegalArgumentException("matrix is broken");
-      }
-      for (int element : arr) {
-        if (element != 0 || element != 1) {
-          throw new IllegalArgumentException("matrix is broken");
-        }
-      }
-    }
+    if (matrix.get(0).size() == 0) return false;
+    return isColumnsTrue(matrix);
   }
 
-  private static boolean isArrayTrue (ArrayList<Integer> arr) {
-    
+  private static boolean isLinesTrue (ArrayList<ArrayList<Integer>> matrix) {
+    if (isListTrue(matrix.get(0))) {
+      return true;
+    }
+    matrix.remove(0);
+    if (matrix.size() == 0) return false;
+    return isLinesTrue(matrix);
+  }
+
+  private static boolean isListTrue (ArrayList<Integer> line) {
+    for (int element : line) {
+      if (element != 0 && element != 1) {
+        throw new IllegalArgumentException("Element !=0 or 1");
+      } else if ((line.get(0) == 0 && element == 1) || (line.get(0) == 1 && element == 0)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
