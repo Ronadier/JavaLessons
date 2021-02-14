eb226fce7ddf5270c8ee5a3cbd1082d21fe46a84
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-14 11:58:22 +0300

    сканирование директорий

---
+++ src/Day4/Task2.java
@@ -0,0 +1,33 @@
+package Day4;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Task2 {
+  public static void main(String[] args) {
+    File path = new File("D:\\");
+    readFiles(path);
+  }
+
+  public static void readFiles(File baseDirectory) {
+    if (!baseDirectory.isDirectory()) {
+      throw new IllegalArgumentException("Не директория");
+    }
+    scanner:
+    for (File file : baseDirectory.listFiles()) {
+      if(file.isFile()) {
+        System.out.print(file.getName() + " | ");
+      } else {
+        System.out.println();
+        System.out.println(file.getAbsolutePath());
+        if (Objects.equals(file.listFiles(), null)) {
+          continue scanner;
+        }
+        readFiles(file);
+      }
+    }
+  }
+
+}
